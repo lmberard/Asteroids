14874abb178986576b97298f426d5d742685c4aa
martina-fi <52054689+martina-fi@users.noreply.github.com>
2019-07-09 18:32:13 -0300

    Update movimientos.h

--- movimientos.h
+++ movimientos.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <math.h>
 #include "config.h"
+#include "objetos.h"
 
 //FUNCIONES UTILIZADAS PARA MOVER LA NAVE--------------------------------------------------------------------------
 //MOVIMIENTOS.C
@@ -12,7 +13,7 @@
 float computar_velocidad(float vi, float a, float dt);
 
 //dada la posición actual pi, la velocidad actual vi y el paso temporal dt devuelva la posición del próximo instante.
-float computar_posicion(float p,float vi,float dt);
+float computar_posicion(float pi,float vi,float dt);
 
 //dado un vector de n coordenadas modifique cada una de sus componentes mediante la adición del par (dx, dy).
 void trasladar_vector(float **v, size_t n, float dx, float dy);
@@ -21,13 +22,11 @@ void trasladar_vector(float **v, size_t n, float dx, float dy);
 void rotar_vector(float **v, size_t n, float angulo);
 
 //Función utilizada para chequear que la nave no se vaya de los límites de la pantalla (mundo cilindrico)
-void verificar_limites_pantalla(float *posicion_x, float *posicion_y);
+void verificar_limites_pantalla(nave_t *nave);
 
 //Inicializa las variables de la nave, utilizada al principio y en cada nueva partida.
-void inicializar_valores(float *posicion_x, float *posicion_y, float *velocidad_x, float *velocidad_y, float *angulo, float *potencia, float *x_anterior);
+void inicializar_valores(fnave_t *nave);
 
-//Se modifican los parametros de la nave en cada iteración segun las teclas que se hayan presionado.
-void modificar_parametros_nave(float *x_anterior, float *posicion_x, float *posicion_y, float *velocidad_y, float *velocidad_x, float *angulo, float *dt, float *potencia);
-
+void computar_pot(nave_t *nave);
 
 #endif // _MOVIMIENTOS_H_
