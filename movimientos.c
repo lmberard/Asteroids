d488bf2a16d3698377961277a85293448cda743f
martina-fi <52054689+martina-fi@users.noreply.github.com>
2019-07-09 18:28:55 -0300

    Update 1 movimientos.c
    Modifique los parametros de entrada de un par de funciones, tambien cambien de lugar un par de llaves jeje y saque la funcion que modificaba lod parametros de la nave ya que al estr trabajandi con el tda nave me pareció conveniente poner todos las funciones de cada tda juntas en su propio modulo.
    
    ADEMAS esta la funcion computar potencia que tiene que hacer dos cosas, por un lado si la persona apreta una tecla le tiene que dar un impulso a la nave y por otro lado la potencia tiene que decrecer en el tiempo. la fx esa esta mal, no se bien como hacer lo del impulso y demas asi que hay que corregirla para que funque bien

--- movimientos.c
+++ movimientos.c
@@ -1,71 +1,76 @@
 #include "movimientos.h"
-
-float computar_velocidad(float vi, float a, float t){
-    return vi + t*a;
+/*Funcion que computa el valor de velocidad instantanea*/
+float computar_velocidad(float vi, float a, float dt)
+{
+    return vi + dt*a;
 }
-
-float computar_posicion(float p,float vi,float t){
-    return p + t*vi;
+/*Funcion que computa el valor de la posicion instantanea*/
+float computar_posicion(float pi,float vi,float dt)
+{
+    return pi + dt*vi;
 }
+/*Funcion que recibe una matriz de 2 columnas y n elementos y traslada estos en un valor x e y respectivamente*/
 
-void trasladar_vector(float **v, size_t n, float dx, float dy){
-	for (int i = 0; i < n; i++) {
+void trasladar_vector(float **v, size_t n, float dx, float dy)
+{
+	for (size_t i = 0; i < n; i++) 
+	{
 		v[i][0] += dx;
 		v[i][1] += dy;
 	}
 }
 
-void rotar_vector(float **v, size_t n, float angulo){
-	
-	float c = cos(angulo);
-	float s = sin(angulo);
-	float xs, yc, xc, ys;
-	
-	for (int i = 0; i < n ; i++) {
-	
-		xs = v[i][0]*s;
-		yc = v[i][1]*c;
-		xc = v[i][0]*c;
-		ys = v[i][1]*s;	
+void rotar_vector(float **v, size_t n, float angulo)
+{
+	size_t i;
+	float x_rotado;
+	float y_rotado;
+	float coseno = cos(angulo);
+	float seno = sin(angulo);
 
-		v[i][1] = xs + yc;
-		v[i][0] = xc - ys;
+	for(i=0;i<n;i++)
+	{
+		x_rotado = v[i][0] * coseno - v[i][1] * seno;
+		y_rotado = v[i][0] * seno + v[i][1] * coseno;
+		v[i][0] = x_rotado;
+		v[i][1] = y_rotado;
 	}
 }
 
-void verificar_limites_pantalla(float *posicion_x, float *posicion_y){
-	if(*posicion_x >= VENTANA_ANCHO)
-		*posicion_x = 10;
+
+void verificar_limites_pantalla(nave_t *nave)
+{	
+	if(nave->posicion_x >= VENTANA_ANCHO)
+		nave->posicion_x = 10;
 				
-	if(*posicion_x <= 0)
-		*posicion_x = VENTANA_ANCHO-10;
+	if(nave->posicion_x <= 0)
+		nave->posicion_x = VENTANA_ANCHO-10;
 
-	if(*posicion_y >= VENTANA_ALTO)
-		*posicion_y = 10;
+	if(nave->posicion_y >= VENTANA_ALTO)
+		nave->posicion_y = 10;
 
-	if(*posicion_y <=0)
-		*posicion_y = VENTANA_ALTO -10;
+	if(nave->posicion_y <=0)
+		nave->posicion_y = VENTANA_ALTO -10;
 }
 
 //void inicializar_variables(float posicion_x, float posicion_y, float velocidad_x, float velocidad_y, float angulo, float x_anterior){}
-void inicializar_valores(float *posicion_x, float *posicion_y, float *velocidad_x, float *velocidad_y, float *angulo, float *potencia, float *x_anterior){
-	
-	*potencia = (float) NAVE_POTENCIA_INICIAL;
-	*posicion_x = (float) NAVE_X_INICIAL;
-	*posicion_y = (float) NAVE_Y_INICIAL;
-	*velocidad_x = (float) NAVE_VX_INICIAL;
-	*velocidad_y = (float) NAVE_VY_INICIAL;
-	*angulo = (float) NAVE_ANGULO_INICIAL;
-	*x_anterior = 0;
+void inicializar_valores(nave_t *nave)
+{	
+	nave->potencia = (float) NAVE_POTENCIA_INICIAL;
+	nave->posicion_x = (float) NAVE_X_INICIAL;
+	nave->posicion_y = (float) NAVE_Y_INICIAL;
+	nave->velocidad_x = (float) NAVE_VX_INICIAL;
+	nave->velocidad_y = (float) NAVE_VY_INICIAL;
+	nave->angulo = (float) NAVE_ANGULO_INICIAL;
 }
 
-void modificar_parametros_nave(float *x_anterior, float *posicion_x, float *posicion_y, float *velocidad_y, float *velocidad_x, float *angulo, float *dt, float *potencia){
-			
-			*x_anterior = *posicion_x;
-			
-			*velocidad_y = computar_velocidad(*velocidad_y, *potencia*cos(*angulo), *dt);
-			*velocidad_x = computar_velocidad(*velocidad_x, *potencia*sin(-*angulo), *dt);
 
-			*posicion_y = computar_posicion(*posicion_y, *velocidad_y, *dt);
-			*posicion_x = computar_posicion(*posicion_x, *velocidad_x, *dt);
+void computar_pot(nave_t *nave)//Esta funcion está medio mal medio bien, hay que pulirla tal que la potencia sea solo un IMPULSO
+{
+	//nave->potencia -= nave->potencia * 0.1;
+	nave->potencia = nave->potencia * 1000;
+	if(nave->potencia <=0)
+		nave->potencia=NAVE_POTENCIA_INICIAL;
+	if (nave->potencia >= NAVE_MAX_POTENCIA)
+		nave->potencia = NAVE_MAX_POTENCIA;
 }
