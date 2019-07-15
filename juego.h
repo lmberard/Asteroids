d76323faa2c3c3a62df4c8358ef4f4a93b6ccf41
luliberard <50753891+luliberard@users.noreply.github.com>
2019-07-15 01:52:18 -0300

    Create juego.h
    Son todas funciones que tienen que ver con el funcionamiento del juego. 
    son bocha asique supongo que todavia se podria mejorar pero por ahora las pongo todas aca total faltan cosas.
    
    ((En cuanto a los nombres de las funciones: son bastantes pedorros pero no se me ocurria otra cosa, podes cambiarlos si te parecen rancios jeje ))
    -----------------------------------------------------------------------------------
    1)Para las funciones nave-asteroides:
    La funcion "importante" es la de colisiono_nave_asteroide(). Ahi lo que pense fue comparar la posicion de la nave con la de los asteroides, para eso tuve que iterar la lista de asteroides y para cada asteroide use la funcion nave_choco_con_asteroide().
    Cuando la nave choca con el asteroide, se tienen que destruir ambas cosas y crear dos asteroides mas chicos. 
    El problema es que ademas de eso se pierden vidas y segun la cantidad de vidas, se juega de nuevo o game over y podes jugar otra partida. (mas info sobre esto en el .c)
    
    2)Para las funciones disparos-asteroides:
    Aca la importante es analizar_disparos(). Lo que pense fue iterar primero los disparos y comparar cada uno con toda la lista de asteroides (como si fuese un for adentro de otro for)

---
+++ juego.h
@@ -0,0 +1,45 @@
+#ifndef _JUEGO_H_
+#define _JUEGO_H_
+
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+#include "config.h"
+#include "movimientos.h"
+#include "graficador.h"
+#include "objetos.h"
+#include "lista.h"
+#include "iterador.h"
+
+////FUNCIONES PARA ANALIZAR SI LA NAVE CHOCO CON ALGUN ASTEROIDE------------
+
+//Esta compara la nave con un solo asteroide
+bool nave_choco_con_asteroide(nave_t *nave, asteroide_t *asteroide);
+
+//Esta funcion compara la posicion de la nave con la de cada asteroide, devuelve true si choca con alguno.
+bool colisiono_nave_asteroides(nave_t *nave, lista_t *lista_asteroides);
+
+//PROBLEMA
+void comenzar_nueva_vida(nave_t *nave, lista_t *lista_asteroides, SDL_Renderer *renderer);
+
+//PROBLEMAA
+void comenzar_nueva_partida();
+
+
+
+///FUNCIONES PARA VERIFICAR SI EL DISPARO LE PEGO AL ASTEROIDE----------------
+
+//Funcion que devuelve true si el disparo se encuentra en una posicion cercana a un solo asteroide(lo choca)
+bool disparo_choca_al_asteroide(disparo_t *disparo, asteroide_t *asteroide);
+ 
+//Funcion que analiza si el disparo le pego a algun asteroide, de ser asi los destruye y asigna puntaje
+void analizar_disparos(lista_t *lista_disparo, lista_t *lista_asteroides, float *puntos);
+
+//Asigna un puntaje segun el tipo de asteroide que se haya disparado.
+void asignar_puntaje(asteroide_t *asteroide, float *puntos);
+
+
+//Si no hay mas asteroides en la pantalla, esta funcion genera nuevos asteroides pero cada vez que pasa se generan con mas cantidad.
+void generar_nuevos_asteroides(lista_t *lista_asteroide, int cant_asteroides);
+
+
+#endif 
