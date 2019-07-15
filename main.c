190085f5f3f3fa19af5ba3cc57a898030e4b72a0
luliberard <50753891+luliberard@users.noreply.github.com>
2019-07-15 02:18:57 -0300

    Update main.c
    Inclui lo de juego.h 
    Igual esta todo comentado por las dudas porque hay cosas que no estan completas

--- main.c
+++ main.c
@@ -10,9 +10,10 @@
 #include "lista.h"
 #include "disparo.h"
 #include "asteroide.h"
+//#include "juego.h"
 
-int main() {
-	//giladas para que aparezca la pantalla del juego
+int main() 
+{
 	SDL_Init(SDL_INIT_VIDEO);
 
 	SDL_Window *window;
@@ -26,21 +27,26 @@ int main() {
 	
 	if(graficador_inicializar(ARCHIVO_BIN,VENTANA_ANCHO, VENTANA_ALTO))//Aca levanto todos los sprites del archivo binario
 		return EXIT_FAILURE;
+	
 	//zona de variables-----------------
 	nave_t nave;
 	float dt = 1/(float)JUEGO_FPS;
 	float tiempo = 0;
 	float puntos = 0;
+	//Tendria que hacer un vector dinamico donde vaya guardando los puntajes y una funcion que me diga cual de todos es el mejor
+	float mejor_puntaje = 0; //pero mientras uso esto jeje lol
 
 	inicializar_valores(&nave);//Inicializo los valores de la nave
 	lista_t *lista_disparo = lista_crear(); //creo la lista de disparos,debo eliminarla al final		
 	lista_t *lista_asteroides = lista_crear();
 	int vidas_disponibles = CANT_VIDAS_INICIAL;
+	int cant_asteroides = CANT_ASTEROIDES_INICIAL;
 
-	for(size_t i=0; i<=CANT_ASTEROIDES_INICIAL; i++)//Aca estoy creando los asteroides que aparecen apenas empieza el juego
+	for(size_t i=0; i<= cant_asteroides; i++)//Aca estoy creando los asteroides que aparecen apenas empieza el juego
 	{	
 		crear_asteroide(lista_asteroides);
 	}
+	//aca se podria usar la funcion de generar_nuevos_asteroides()
 
 	//end zona de variables-------------------------------
 	unsigned int ticks = SDL_GetTicks();
@@ -55,12 +61,11 @@ int main() {
 						nave.potencia+= INCREMENTO_POTENCIA;
 						break;
 					case SDLK_SPACE:
-						crear_disparo(lista_disparo,nave);//Esta funcion crea un disparo y lo agrega a la lista de disparos
+						crear_disparo(lista_disparo,nave);
 						break;
 					case SDLK_RIGHT:				
 						nave.angulo -= NAVE_ROTACION_PASO;
 						break;
-
 					case SDLK_LEFT:					
 						nave.angulo += NAVE_ROTACION_PASO;
 						break;
@@ -76,22 +81,32 @@ int main() {
 			// BEGIN código del alumno-------------------------------------
 			nave_mover(&nave, dt);//Muevo la nave en un fx que modifica todos sus parametros
 			
-			disparos_modificar(lista_disparo,dt,renderer);//Esta fx modifica los parametros de todos los disparos y en simulatneo los grafica, esto lo hice asi para recorrer la lista una sola vez en vez de varias veces
+			disparos_modificar(lista_disparo,dt,renderer);
+			//Esta fx modifica los parametros de todos los disparos y en simulatneo los grafica, esto lo hice asi para recorrer la lista una sola vez en vez de varias veces
 			asteroide_modificar(lista_asteroides,dt,renderer);
-			
-			tiempo += dt;
 
 			////////////////ZONA DE DIBUJO/////////////////
 			if(nave_dibujar(&nave, renderer)==false)
 				break;
-		
-			dibujar_parametros(puntos, tiempo, caracteres, tam_caracteres, renderer);
+			/*
+			if(colisiono_nave_asteroide(&nave, lista_asteroides)
+			{
+				vidas_disponibles --;
+				if(hay_vidas_disponibles(vidas_disponibles))
+					comenzar_nueva_vida(&nave, lista_asteroides);
+				else
+					comenzar_nueva_partida();
+			}
+			*/
+			//aca de nuevo el problema del mejor_puntaje:
+			dibujar_parametros(puntos, mejor_puntaje, caracteres, tam_caracteres, renderer);
+			
 			dibujar_vidas(&nave, renderer,vidas_disponibles);
 			////////////////////////////////////////////////
 			
 			
 		// END código del alumno----------------------------------------------
-        SDL_RenderPresent(renderer);
+        	SDL_RenderPresent(renderer);
 		ticks = SDL_GetTicks() - ticks;
 		if(dormir) {
 			SDL_Delay(dormir);
